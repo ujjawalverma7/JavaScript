c9c06c50e60026bfe482424d3a18f9f4faf3641c
ujjawalverma7 <147232300+ujjawalverma7@users.noreply.github.com>
2025-06-14 12:07:29 +0000

    config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1223344
+let accountEmail = "ujjawal@gmail.com"
+var accountPassword = "12334"
+accontCity = "Delhi"
+let accountState ;
+
+// accountId = 2 //not allowed
+
+accountEmail ="fsdf.com"
+accountPassword = "3244"
+accountCity = "Goa"
+
+ 
+console.log([accountId, accountEmail, accountPassword, accountCity, accountState]);
