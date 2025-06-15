838a1f705f05410e9fe247cd310316c471e568f6
ujjawalverma7 <147232300+ujjawalverma7@users.noreply.github.com>
2025-06-15 03:00:09 +0000

    operations

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,24 @@
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+
+// let str1 = "Hello"
+// let str2 = " Ujjawal"
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 +"4");
+
