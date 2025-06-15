91bb40f24958a614004c4a318387fd2855246c1c
ujjawalverma7 <147232300+ujjawalverma7@users.noreply.github.com>
2025-06-15 02:17:57 +0000

    Converion of datatypes

---
+++ 01_basics/02_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = 33;
+let score1 = "33";
+let score2 = String("33abc");
+let score3 = Number(true);
+
+console.log(typeof score);
+console.log(typeof score1);
+console.log(typeof score2);
+console.log(typeof score3);
+
+console.log(score);
+console.log(score1);
+console.log(score2);
+console.log(score3);
+
+
+// "33" => 33
+//"33abc" => NaN
+//true => 1 
+//false => 0
+
+// boolean conversion
+
+// "" => false
+// "Ujjawal" => true 
+
